230cc254b1276102daf95d02df025692a6bb9a79
Daniel Bacsur <contact@danielbacsur.com>
2024-08-06 00:45:17 +0200

    chore: configure google generative ai with api key

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+import dotenv
 import datasets
+import google.generativeai as genai
+
+dotenv.load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=GEMINI_API_KEY)
 
 dataset = datasets.load_dataset("HuggingFaceM4/datikz")
