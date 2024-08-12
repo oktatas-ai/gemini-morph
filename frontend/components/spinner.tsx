6701f150bfaaf33c94030c252545fbe94d595fc1
Daniel Bacsur <contact@danielbacsur.com>
2024-08-12 20:53:49 +0200

    refactor: update spinner component to use tailwind

--- frontend/components/spinner.tsx
+++ frontend/components/spinner.tsx
@@ -1,22 +1,5 @@
 export function Spinner() {
   return (
-    <div className="spinner">
-      <style jsx>{`
-        .spinner {
-          border: 4px solid rgba(0, 0, 0, 0.1);
-          width: 24px;
-          height: 24px;
-          border-radius: 50%;
-          border-left-color: #fff;
-          animation: spin 1s linear infinite;
-        }
-
-        @keyframes spin {
-          to {
-            transform: rotate(360deg);
-          }
-        }
-      `}</style>
-    </div>
+    <div className="border-4 border-opacity-20 border-black w-6 h-6 rounded-full border-l-white animate-spin" />
   );
 }
